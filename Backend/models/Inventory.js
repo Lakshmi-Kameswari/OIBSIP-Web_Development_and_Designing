12c96f17e79e9c6839409b2f0d5d60eb5d898caa
Lakshmi-Kameswari <238x1a4264@khitguntur.ac.in>
2026-03-09 06:52:16 +0530

    Add Inventory model with item and quantity fields

--- Backend/models/Inventory.js
+++ Backend/models/Inventory.js
@@ -1,1 +1,10 @@
+const mongoose = require("mongoose")
 
+const InventorySchema = new mongoose.Schema({
+
+    item:String,
+    quantity:Number
+
+})
+
+module.exports = mongoose.model("Inventory",InventorySchema)
