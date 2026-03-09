2076a4463e5e9ec222fbf39ea25a2bbed160ed44
Lakshmi-Kameswari <238x1a4264@khitguntur.ac.in>
2026-03-09 06:51:54 +0530

    Add Order model with schema definition

--- Backend/models/Order.js
+++ Backend/models/Order.js
@@ -1,1 +1,21 @@
+const mongoose = require("mongoose")
 
+const OrderSchema = new mongoose.Schema({
+
+    userId:String,
+
+    base:String,
+    sauce:String,
+    cheese:String,
+    veggies:[String],
+
+    status:{
+        type:String,
+        default:"Order Received"
+    },
+
+    paymentId:String
+
+})
+
+module.exports = mongoose.model("Order",OrderSchema)
