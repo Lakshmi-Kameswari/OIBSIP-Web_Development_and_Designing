3c8a8e8289a13d386037d2293da0689a573f6f63
Lakshmi-Kameswari <238x1a4264@khitguntur.ac.in>
2026-03-09 06:54:24 +0530

    Implement login functionality in Login component

--- Frontend/src/components/Login.js
+++ Frontend/src/components/Login.js
@@ -1,1 +1,42 @@
+import React, {useState} from "react"
+import axios from "axios"
 
+function Login(){
+
+const [email,setEmail]=useState("")
+const [password,setPassword]=useState("")
+
+const login=async()=>{
+
+const res = await axios.post("http://localhost:5000/api/auth/login",{
+email,password
+})
+
+alert("Login Success")
+
+}
+
+return(
+
+<div className="card">
+
+<h2>Login</h2>
+
+<input placeholder="Email"
+onChange={(e)=>setEmail(e.target.value)}
+/>
+
+<input placeholder="Password"
+type="password"
+onChange={(e)=>setPassword(e.target.value)}
+/>
+
+<button onClick={login}>Login</button>
+
+</div>
+
+)
+
+}
+
+export default Login
